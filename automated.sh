f12cedf74cdb511365ade7f77144ceffa658334d
mohkittaneh <moh.kittaneh1@gmail.com>
2025-01-17 16:08:30 +0000

    Automated commit from script

--- automated.sh
+++ automated.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Variables
 BRANCH="main"
 REPO_URL="github.com:mohkittaneh/dictionary_app.git"
 JENKINS_URL="http://172.20.10.2:8080/job/dictionary_app2/build"
 JENKINS_USER="admin"
 JENKINS_TOKEN="11e16ea07662cc8a0a80a3e667c31846fe"
 
-# Check for uncommitted changes
 if ! git diff-index --quiet HEAD --; then
     echo "Uncommitted changes detected. Stashing changes..."
     git stash
@@ -16,7 +14,6 @@ else
     STASHED=false
 fi
 
-# Pull the latest changes from the remote repository
 echo "Pulling the latest changes from the remote repository..."
 if git pull --rebase; then
     echo "Successfully pulled the latest changes."
@@ -27,7 +24,6 @@ else
     echo "Merge completed."
 fi
 
-# Apply stashed changes if any
 if [ "$STASHED" = true ]; then
     echo "Reapplying stashed changes..."
     git stash pop
@@ -37,14 +33,12 @@ if [ "$STASHED" = true ]; then
     fi
 fi
 
-# Stage and commit any new changes
 if ! git diff-index --quiet HEAD --; then
     echo "Changes detected. Committing and pushing to GitHub..."
     git add .
     git commit -m "Automated commit from script"
 fi
 
-# Push changes to the remote repository
 echo "Pushing changes to the remote repository..."
 if git push; then
     echo "Changes pushed successfully."
@@ -54,12 +48,10 @@ else
     git push
 fi
 
-# Trigger the Jenkins job
 echo "Triggering the Jenkins job..."
 response=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$JENKINS_URL" \
     --user "$JENKINS_USER:$JENKINS_TOKEN")
 
-# Check Jenkins job trigger response
 if [ "$response" -eq 201 ]; then
     echo "Jenkins job triggered successfully!"
 else
