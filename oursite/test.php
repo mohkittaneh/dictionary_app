7a146ef81b460c0356d5fdca24426f8d7a661d7c
mohkittaneh <moh.kittaneh1@gmail.com>
2025-01-20 09:21:52 +0000

    edit

--- oursite/test.php
+++ oursite/test.php
@@ -1,14 +1,27 @@
 <?php
 $servername = "db";
-$username = "user";
-$password = "your_password";
+$username = "root";
+$password = "123";
 
 $conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Database connection successful!";
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $input_word = $conn->real_escape_string($_POST['input_word']);
+    $sql = "SELECT matched_word FROM mydb.words WHERE input_word = '$input_word'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $matched_word = $row['matched_word'];
+        echo json_encode(['success' => true, 'matched_word' => $matched_word]);
+    } else {
+        echo json_encode(['success' => false, 'message' => 'Word not found']);
+    }
+}
+
 $conn->close();
 ?>
